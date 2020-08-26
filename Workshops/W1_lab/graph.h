1ff59e26fd6eeea1d05e49bea8ab759285483b95
bakyt kurmanov <52513370+bakyt87@users.noreply.github.com>
2020-08-26 07:15:14 -0400

    Add files via upload

---
+++ Workshops/W1_lab/graph.h
@@ -0,0 +1,14 @@
+#ifndef SDDS_GRAPH_H__
+#define SDDS_GRAPH_H__
+#define MAX_NO_OF_SAMPLES 20
+#define GRAPH_WIDTH 65
+namespace sdds
+
+{
+	int findMax(int samples[], int noOfSamples);
+	void printBar(int val, int max);
+	void printGraph(int samples[], int noOfSamples);
+	void getSamples(int samples[], int noOfSamples);
+}
+#endif
+#pragma once
---
+++ Workshops/W1_lab/tools.h
@@ -0,0 +1,11 @@
+#ifndef SDDS_TOOLS_H__
+#define SDDS_TOOLS_H__
+namespace sdds
+{
+	int menu(int noOfSamples);
+	void goBack(int n);
+	void line(int n, const char* label = nullptr);
+	int getInt(int min, int max);
+}
+#endif // !SDDS_TOOLS_H__
+#pragma once
